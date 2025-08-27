7edd908c95e70766b037f10ea81a22391103cc17
Anna <wheelercs@protonmail.com>
2025-08-27 08:39:41 -0500

    Added Setup Scripts
    Added setup scripts for bare-metal Windows and Linux project installations.
    
    Signed-off-by: Anna <wheelercs@protonmail.com>

---
+++ windows_setup.ps1
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5.4/npp.8.5.4.Installer.x64.exe -O "ExamplePE.exe"
+mv .\resources\* .\
+rmdir resources\
+python3 -m venv venv
+source .\venv\bin\activate
+pip3 install -r .\requirements.txt
