5a6ce4e0c69dcdd54c4106cbf8b71a700560ef89
Anna <wheelercs@protonmail.com>
2025-08-27 08:41:22 -0500

    Removed Bash Shebang from Windows Script
    Signed-off-by: Anna <wheelercs@protonmail.com>

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 wget https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5.4/npp.8.5.4.Installer.x64.exe -O "ExamplePE.exe"
 mv .\resources\* .\
 rmdir resources\
